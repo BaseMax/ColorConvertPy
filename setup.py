79428e6972d2f3c2467ebc97d7059f18cdbff39a
Max Base <MaxBaseCode@Gmail.Com>
2019-03-31 04:55:51 +0430

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,5 +5,8 @@ with open("README.md", "r") as fh:
 		name='ColorConvert',  
 		version='1.0',
 		scripts=['ColorConvert.py'],
+		author="Max Base",
+		author_email="MaxBaseCode@Gmail.Com",
+		description="Tiny library to convert various colored units (rgb, hex).",
 	)
 
