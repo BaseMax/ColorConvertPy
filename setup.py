a60b49f7c5fd52d4a3524bca708087870173c5c7
Max Base <MaxBaseCode@Gmail.Com>
2019-03-31 04:56:24 +0430

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,12 +5,17 @@ with open("README.md", "r") as fh:
 		name='ColorConvert',  
 		version='1.0',
 		scripts=['ColorConvert.py'],
+		url="https://github.com/BaseMax/ColorConvertPy",
+		packages=setuptools.find_packages(),
 		author="Max Base",
 		author_email="MaxBaseCode@Gmail.Com",
 		description="Tiny library to convert various colored units (rgb, hex).",
 		long_description=Description,
 		long_description_content_type="text/markdown",
-		url="https://github.com/BaseMax/ColorConvertPy",
-		packages=setuptools.find_packages(),
+		classifiers=[
+			"Programming Language :: Python :: 2",
+			"License :: OSI Approved :: MIT License",
+			"Operating System :: OS Independent",
+		],
 	)
 
