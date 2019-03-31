bbeed0735fb603c6535bacd36e93ab163872079e
Max Base <MaxBaseCode@Gmail.Com>
2019-03-31 04:56:00 +0430

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,5 +8,9 @@ with open("README.md", "r") as fh:
 		author="Max Base",
 		author_email="MaxBaseCode@Gmail.Com",
 		description="Tiny library to convert various colored units (rgb, hex).",
+		long_description=Description,
+		long_description_content_type="text/markdown",
+		url="https://github.com/BaseMax/ColorConvertPy",
+		packages=setuptools.find_packages(),
 	)
 
