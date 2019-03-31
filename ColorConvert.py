562153c987f911dd3d08337c4231763897cfabb8
Max Base <MaxBaseCode@Gmail.Com>
2019-03-31 04:37:23 +0430

    Update ColorConvert.py

--- ColorConvert.py
+++ ColorConvert.py
@@ -1,2 +1,15 @@
 def rgb2hex(colors):
     return f"#{''.join(f'{hex(item)[2:].upper():0>2}' for item in colors)}"
+
+def rgb2hex(colors):
+    result = '#'
+    for value in colors:
+       # 0x6e
+       hex_string = hex(value)
+       # 6e
+       hex_string = hex_string[2:]
+       # 6E
+       hex_string = hex_string.upper()
+       # #6E...
+       result += hex_string
+    return result
