a005b78861c46eb202ea710993a0c13db706e15b
Max Base <MaxBaseCode@Gmail.Com>
2019-03-31 04:55:40 +0430

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,6 +2,8 @@ import setuptools
 with open("README.md", "r") as fh:
 	Description = fh.read()
 	setuptools.setup(
-
+		name='ColorConvert',  
+		version='1.0',
+		scripts=['ColorConvert.py'],
 	)
 
