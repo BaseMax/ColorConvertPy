59bbe401a0c81ef6da05e0d6bac98d93914fcfca
Max Base <MaxBaseCode@Gmail.Com>
2019-03-31 04:44:49 +0430

    Update ColorConvert.py

--- ColorConvert.py
+++ ColorConvert.py
@@ -1,8 +1,17 @@
-def rgb2hex(colors):
-	return "%s" % (hex(colors) [2:]).upper()
-
-def rgb2hex(colors):
-	result = '#'
-	for value in colors:
+def rgb2hex(value):
+	# return "%s" % (hex(value) [2:]).upper()
+	# result = '#'
+	result = ''
+	for value in value:
 		result += hex(value)[2:].upper()
 	return result
+
+def hex2rgb(value):
+	value = int(value, 16)
+	hex = 2**8
+	blue = value % hex
+	value = value >> 8
+	green = value % hex
+	value = value >> 8
+	red = value % hex
+	return (red, green, blue)
